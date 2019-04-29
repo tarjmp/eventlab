2ab14bdcd3c50f1b1f42a88f7c0cb221058912d3
tarjmp <ben.samtleben@gmail.com>
2019-04-29 14:01:18 +0200

    Extended test login functionality

---
+++ tests/Unit/QueryTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class QueryTest extends TestCase
+{
+    /**
+     * Test for Query class (app/Tools/Query)
+     *
+     * @return void
+     */
+    public function testGetUserEvents()
+    {
+        $this->assertTrue(true);
+    }
+}
