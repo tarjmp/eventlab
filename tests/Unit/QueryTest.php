65cee432013c2364b7189bcb7a8fd68f8be7f78d
tarjmp <ben.samtleben@gmail.com>
2019-04-29 15:40:06 +0200

    First try query test

--- tests/Unit/QueryTest.php
+++ tests/Unit/QueryTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\Unit;
 
+use App\Tools\Query;
+use App\User;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class QueryTest extends TestCase
 {
+
+    // this array contains an array of events for each user (including private events, subscriptions, memberships)
+    private const USERS_EVENTS = [[]];
+
     /**
      * Test for Query class (app/Tools/Query)
      *
@@ -15,6 +19,28 @@ class QueryTest extends TestCase
      */
     public function testGetUserEvents()
     {
-        $this->assertTrue(true);
+        $user = new User();
+
+        // iterate over all users
+        for($i = 0; $i < count(self::USERS_EVENTS); $i++) {
+
+            // pseudo login with the current user id
+            $user->id = $i + 1;
+            $this->loginWithFakeUser($user);
+
+            // call the function to be tested and only save the column 'id' of the events
+            $aTestResult = array_column(Query::getUserEvents(), 'id');
+
+            // iterate over all events
+            for($k = 0; $k < 10; $k++) {
+
+                if(in_array($k, self::USERS_EVENTS[$i], true)) {
+                    $this->assertTrue(in_array($k, $aTestResult, true));
+                }
+                else {
+                    $this->assertNotTrue(in_array($k, $aTestResult, true));
+                }
+            }
+        }
     }
 }
