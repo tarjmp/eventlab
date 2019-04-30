57a0c5778354e4469b60ca3a2c3e3bab3935b913
luukaass <qualo.lukas@student.dhbw-karlsruhe.de>
2019-04-30 12:06:25 +0200

    Implemented further tests

--- tests/Unit/PermissionTest.php
+++ tests/Unit/PermissionTest.php
@@ -30,6 +30,26 @@ class PermissionTest extends TestCase
         $this->assertTrue(Permission::has(Permission::showGroup, 6));
     }
 
+    public function testHasShowGroupExtended()
+    {
+        //User not logged in and private Group
+        $this->assertFalse(Permission::has(Permission::showGroupExtended, 2));
+
+        //User not logged in and public Group
+        $this->assertFalse(Permission::has(Permission::showGroupExtended, 1));
+
+        //User logged in and private Group he is not member
+        $this->loginWithDBUser(6);
+        $this->assertFalse(Permission::has(Permission::showGroupExtended, 6));
+
+        //User logged in and private Group he is member
+        $this->loginWithDBUser(8);
+        $this->assertTrue(Permission::has(Permission::showGroupExtended, 6));
+
+        //User logged in and public Group
+        $this->assertTrue(Permission::has(Permission::showGroupExtended, 6));
+    }
+
 
     //Methods Check test
 
