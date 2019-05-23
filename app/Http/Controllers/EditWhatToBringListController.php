5dba3ed4ea8f891eead9942b04c2d0357b7cd684
luukaass <qualo.lukas@student.dhbw-karlsruhe.de>
2019-05-23 21:12:43 +0200

    Initial version of controller for the What-to-bring list

---
+++ app/Http/Controllers/EditWhatToBringListController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tools\PermissionFactory;
+
+class EditWhatToBringListController extends Controller
+{
+
+    //Show the form of the What-to-bring list
+    public function show($id)
+    {
+
+        //check if the user is allow to see this list
+        PermissionFactory::createShowEventExtended()->check($id);
+
+        //retrieve the information stored in the database
+        $items = array();
+
+        return view('what_to_bring_list_show')->with(['items' => $items]);
+    }
+
+}
