cf60d755bc60004ee0d18725f3b2035578205ddd
AnettR <anett.richel@web.de>
2019-05-25 22:38:44 +0200

    Added search result view

---
+++ resources/views/search.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <h2>{{ __('search.title') }}</h2>
+
+            </div>
+        </div>
+    </div>
+@endsection
