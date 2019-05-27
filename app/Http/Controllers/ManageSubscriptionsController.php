f83980e0388ef9d0c64399b96c26b265e714753b
luukaass <qualo.lukas@student.dhbw-karlsruhe.de>
2019-05-27 23:24:50 +0200

    Fixed issues

--- app/Http/Controllers/ManageSubscriptionsController.php
+++ app/Http/Controllers/ManageSubscriptionsController.php
@@ -20,27 +20,9 @@ class ManageSubscriptionsController extends Controller
 
     public function update(Request $request)
     {
+        //TODO delete subscription
 
-        // check for permission to edit the event
-        //PermissionFactory::createEditEvent()->check();
-
-        $data = $request->all();
-        // never trust any user input
-        $this->validateInput($data);
-
-        $id = $data['eventID'];
-
-        // Update the event with passed data
-        $item = new Item();
-        $item->event_id = $id;
-        $item->name = $data['name'];
-        $item->amount = $data['amount'];
-        if (isset($data['user'])) $item->user_id = Auth::user()->id;
-        else $item->user_id = null;
-
-        $item->save();
-
-        return view('what-to-bring-list-show')->with(['eventID' => $id, 'updated' => true, 'items' => $this->getItems($id)]);
+        return view('manage-subscriptions')->with(['items' => $this->getItems()]);
     }
 
     private function getItems()
@@ -48,13 +30,6 @@ class ManageSubscriptionsController extends Controller
         //retrieve the information stored in the database
         $user = Auth::user();
         $items = $user->groups(Group::TYPE_SUBSCRIPTION);
-
-        foreach($items as $item)
-        {
-            echo $item;
-        }
-
-        die();
 
         return $items;
     }
