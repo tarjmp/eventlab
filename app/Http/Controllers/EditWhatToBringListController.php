6b789943011d1d7fb7ea58d82a65a24c81b757b7
luukaass <qualo.lukas@student.dhbw-karlsruhe.de>
2019-05-27 10:51:35 +0200

    Improved database query

--- app/Http/Controllers/EditWhatToBringListController.php
+++ app/Http/Controllers/EditWhatToBringListController.php
@@ -21,14 +21,12 @@ class EditWhatToBringListController extends Controller
         $items = $event->items;
 
         foreach ($items as $item) {
-            if (isset($item->user_id)) {
-                $user = User::find($item->user_id);
-                $item['full_name'] = $user->name();
+            if (isset($item->user)) {
+                $item['full_name'] = $item->user->name();
             } else {
                 $item['full_name'] = null;
             }
         }
-
         return view('what_to_bring_list_show')->with(['items' => $items]);
     }
 
