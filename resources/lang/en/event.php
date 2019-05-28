a806d45cdea8c0ce3b313b234d0f2fed85f325ea
tarjmp <ben.samtleben@gmail.com>
2019-05-28 02:16:07 +0200

    Fixed event reply texts

--- resources/lang/en/event.php
+++ resources/lang/en/event.php
@@ -35,7 +35,9 @@ return [
     'notifications_tentative' => 'Tentative',
     'notifications_text'      => 'You were added to the Event ":name". Do you want to accept and join the event?',
     'details_text'            => '(To get more details click here)',
-    'no_notifications'        => 'No notifications found.',
-    'replied'                 => 'You have replied to the event ":name" with the status :status.'
+    'no_notifications'        => 'No notifications. All done!',
+    'replied_accepted'        => 'You accepted event ":name".',
+    'replied_rejected'        => 'You rejected event ":name".',
+    'replied_tentative'       => 'You accepted event ":name" tentatively.',
 
 ];
